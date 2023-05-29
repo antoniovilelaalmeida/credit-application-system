6e5d14e68f836adb7d229fb3b054a1485df2b226
Antonio vilela <antoniovilelaalmeida@hotmail.com>
2023-05-29 18:15:12 -0300

    Adicionando arquivos de repositorio jpa

---
+++ src/main/kotlin/medio/creditapplicationsystem/entity/Address.kt
@@ -0,0 +1,11 @@
+package medio.creditapplicationsystem.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Embeddable
+
+@Embeddable
+data class Address(
+   @Column(nullable = false) var zipcode: String = "",
+   @Column(nullable = false) var street: String = ""
+)
+
